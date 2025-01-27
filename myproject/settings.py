8003772ec41817f2a414276fe13c30b2b47eb7ab
mbpro <webebuka@gmail.com>
2025-01-27 02:42:56 +0100

    Whitenoise commit2

--- myproject/settings.py
+++ myproject/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoisemiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'myproject.urls'
