4ce2f667629bbc4bb3dcf8c88fa9fa5faf22a39b
mbpro <webebuka@gmail.com>
2025-01-27 02:54:15 +0100

    Host address

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ql0nrty^fk%#x3&8ff*&4xrt)#y^+3-7)1(fnx9pi&shpt4p#)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://djecommerce.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
