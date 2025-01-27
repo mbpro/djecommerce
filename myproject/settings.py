fd8a783b43bc7c19f8097e0deb08a852eb63a463
mbpro <webebuka@gmail.com>
2025-01-27 02:26:13 +0100

    White noise correction

--- myproject/settings.py
+++ myproject/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.Whitenoisemiddle'
+    'whitenoise.middleware.WhiteNoisemiddleware',
 ]
 
 ROOT_URLCONF = 'myproject.urls'
